81432a799bd12f730296481f12380d54879dd3ca
hustonwehaveabug <75804093+hustonwehaveabug@users.noreply.github.com>
2020-12-12 20:21:39 +0100

    task 2 part 2

--- src/main/java/org/example/task2/Part2.java
+++ src/main/java/org/example/task2/Part2.java
@@ -4,31 +4,27 @@ import org.example.utils.FileReader;
 
 import java.util.List;
 
-public class Part1 {
+public class Part2 {
 
     public static void main(String[] args) {
 
         List<String> input = FileReader.readFileAsStrings("task2-input.txt");
         long validPasswordsCount = input.stream()
                 .map(s -> s.trim().split("\\s+"))
-                .map(Part1::validatePass)
+                .map(Part2::validatePass)
                 .filter(b -> b)
                 .count();
         System.out.println("Valid passwords: " + validPasswordsCount);
     }
 
     private static boolean validatePass(String[] split) {
-        String[] range = split[0].split("-");
-        int rangeFrom = Integer.parseInt(range[0]);
-        int rangeTo = Integer.parseInt(range[1]);
+        String[] positions = split[0].split("-");
+        int position1 = Integer.parseInt(positions[0]);
+        int position2 = Integer.parseInt(positions[1]);
         char letterToCheck = split[1].substring(0, 1).toCharArray()[0];
         char[] pass = split[2].toCharArray();
-        int count = 0;
-        for (char letter : pass) {
-            if(letter == letterToCheck) {
-                count++;
-            }
-        }
-        return count >= rangeFrom && count <= rangeTo;
+        char char1 = pass[position1 - 1];
+        char char2 = pass[position2 - 1];
+        return (char1 == letterToCheck || char2 == letterToCheck) && char1 != char2;
     }
 }
